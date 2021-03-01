4de49aea9236fead972a413a93b5242df2c4a532
ObiWanWheeler <charliestinhorto@gmail.com>
2021-03-01 13:41:46 +0000

    connect database

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ import { handleRoot } from './controllers/root.js';
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'DivineHD1',
-      database : 'smartbrain'
+      host : process.env.DATABASE_URL,
+      ssl: true
     }
 });
 
